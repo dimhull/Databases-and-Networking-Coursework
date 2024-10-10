edbec95dcb85a32eed1e056153801e011c74859d
882369 <882369@FEN-052-A-16>
2024-10-10 10:49:28 +0100

    DB API commit

---
+++ UserManagementAPI/UserManagementAPI/Controllers/UsersController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly UserManagementContext _context;
+
+    public UsersController(UserManagementContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/users
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    // GET: api/users/{id}
+    [HttpGet("{id}")]
+    public async Task<ActionResult<User>> GetUser(string id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        return user;
+    }
+
+    // POST: api/users
+    [HttpPost]
+    public async Task<ActionResult<User>> CreateUser(User user)
+    {
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetUser), new { id = user.UserID }, user);
+    }
+
+    // DELETE: api/users/{id}
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(string id)
+    {
+        var user = await _context.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        _context.Users.Remove(user);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
---
+++ UserManagementAPI/UserManagementAPI/Models/UserManagerContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class UserManagementContext : DbContext
+{
+    public UserManagementContext(DbContextOptions<UserManagementContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserEmail> UserEmails { get; set; }
+    public DbSet<UserPosition> UserPositions { get; set; }
+}
+
+
+public class User
+{
+    public string UserID { get; set; }
+    public string LoginID { get; set; }
+    public string Surname { get; set; }
+    public string Forenames { get; set; }
+    public string Title { get; set; }
+    public string Phone { get; set; }
+    public string Location { get; set; }
+}
+
+public class UserEmail
+{
+    public int EmailID { get; set; }
+    public string UserID { get; set; }
+    public string Email { get; set; }
+}
+
+public class UserPosition
+{
+    public int PositionID { get; set; }
+    public string UserID { get; set; }
+    public string Position { get; set; }
+}
